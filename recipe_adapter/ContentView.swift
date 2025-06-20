50b050818599cc5212a7ffd6c06125fd5594215c
asdf6921 <gaojiayi.jerry@gmail.com>
2025-06-20 15:27:32 -0700

    Initial Commit

---
+++ recipe_adapter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  recipe_adapter
+//
+//  Created by Jerry on 6/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world test!!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ recipe_adapter/recipe_adapterApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipe_adapterApp.swift
+//  recipe_adapter
+//
+//  Created by Jerry on 6/20/25.
+//
+
+import SwiftUI
+
+@main
+struct recipe_adapterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
